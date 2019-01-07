7f3f772965a9c5ef3adb0d64351cc66a1e610585
Hunter Fuller <hfuller@pixilic.com>
2019-01-07 01:34:25 -0600

    Add very barebones page generation script using j2cli

---
+++ generate.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euf
+
+pwd
+SLUGS="$(ls img | grep -E '\.jpg$' | sed -e 's/.*\///' -e 's/\..*//' | xargs)"
+echo "We will generate: $SLUGS"
+
+for SLUG in $SLUGS; do
+	echo -n "$SLUG "
+
+	export thing_text="$(cat text/$SLUG.html | xargs)"
+	export thing_slug="$SLUG"
+	j2 template.j2 > "$SLUG.html"
+
+done
+echo "done."
