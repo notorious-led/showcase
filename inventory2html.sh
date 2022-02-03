1f33711c7ac9a661755b81017d137fcd3250b24c
Hunter Fuller <hfuller@pixilic.com>
2022-02-03 00:22:55 -0600

    Add blurbs to inventory page

--- inventory2html.sh
+++ inventory2html.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo '<p>Here are the contents of the vending machine, as of <span class="gendate">the last time it was restocked</span>. Tap on one of the items to reveal details about the piece and its creator.</p>'
+
 cat "$1" | 
 	grep -vE "^ID\W" |
 	while read LINE; do
@@ -12,3 +14,4 @@ cat "$1" |
 		echo "<details><summary><strong>$SLOT ($PRICE)</strong> <span class="name">"$THING"</span></summary><p>$DESC</p><p class="deets">$MEDIA created by $VENDOR</p></details>";
 	done
 
+echo '<p>Feel free to <a href="vending.html">read more about the vending machine</a>, especially if you want to sell your wares. And you can also <a href="/">check out our other projects</a>.'
